a9924500a2a09916c7f2c43ca84bdb5c1f065717
Alexandre <alexandre.culty@iut-valence.fr>
2014-02-17 17:44:19 +0100

    Initial commit.

---
+++ src/Appli.java
@@ -0,0 +1,8 @@
+public class Appli {
+
+    public static void main(String[] args) {
+	Jeux Grille = new Jeux(); // Nouveau jeu
+    }
+    
+
+}
---
+++ src/Jeux.java
@@ -0,0 +1,23 @@
+/**
+ * @author culty & prinsac
+ */
+public class Jeux {
+	
+    private int tableau[][] = {{0,0,0},{0,0,0},{0,0,0}}; // Tableau représentant la grille
+    private int joueur1 = 1; // Numéro du joueur 1
+    private int joueur2 = 2; // Numéro du joueur 2
+    private boolean tour = true; //tour=true: le joueur 1 joue, tour=false: le joueur 2 joue
+    
+    public void Jeux(){
+    }
+ 
+    public void poserPion(){
+	if (tour == true){
+	    tableau[][] = {{0,0,0},{0,0,0},{0,0,0}};
+	    tour = !tour;
+	}
+	else{
+	    
+	}
+    }
+}
