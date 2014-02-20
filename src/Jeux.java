83189c23e09be0de288294978aafda4ff3567477
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-20 23:44:05 +0100

    Add TODO (Jeux.java)

--- src/Jeux.java
+++ src/Jeux.java
@@ -3,22 +3,28 @@
  * @author culty & prinsac
  */
 public class Jeux {
-	
-    private int tableau[][] = {{0,0,0},{0,0,0},{0,0,0}}; // Tableau représentant la grille
-    private int joueur1 = 1; // Numéro du joueur 1
-    private int joueur2 = 2; // Numéro du joueur 2
-    private boolean tour = true; //tour=true: le joueur 1 joue, tour=false: le joueur 2 joue
-    
-    public void Jeux(){
+	/* TODO Pourquoi "0" ? */
+    private int[][] tableau = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; // Tableau représentant la grille
+    /* TODO Pourquoi pas une constante ? */
+    private int     joueur1 = 1; // Numéro du joueur 1
+    /* TODO Pourquoi pas une constante ? */
+    private int     joueur2 = 2; // Numéro du joueur 2
+    private boolean tour    = true; //tour=true: le joueur 1 joue, tour=false: le joueur 2 joue
+
+    public void Jeux() {
     }
- 
-    public void poserPion(){
-	if (tour == true){
-	    tableau[][] = {{0,0,0},{0,0,0},{0,0,0}};
-	    tour = !tour;
-	}
-	else{
-	    
-	}
+
+    public void poserPion() {
+        /* TODO Pourquoi pas simplement tester "tour" plutôt que "tour == true" */
+        if (tour == true) {
+            /* TODO Vous réinitialisez le tableau à chaque coup ? */
+            /* TODO Ce n'est pas du java. */
+            tableau[][]={{0, 0, 0},{0, 0, 0},{0, 0, 0}} ;
+            tour = !tour;
+        }
+        /* TODO Vide ? */
+        else {
+
+        }
     }
 }
