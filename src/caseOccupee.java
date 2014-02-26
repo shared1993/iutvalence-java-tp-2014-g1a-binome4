69ca0cea1d8c60e7d7e4420f2d8e1c92901a33f2
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:33:39 +0100

    Add TODO (caseOccupee.java)

--- src/caseOccupee.java
+++ src/caseOccupee.java
@@ -1,3 +1,6 @@
+/* TODO Javadoc */
+/* TODO Package. */
+/* TODO Nom de classe avec des majuscule en première lettre. */
 public class caseOccupee extends Exception {
-
+    /* TODO En Java, on préférera mettre des Exceptions avec message pour la traçabilité. */
 }
