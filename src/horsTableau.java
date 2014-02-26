a7013734f6bd5319f6a8a6884d03407b9a9e400a
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:33:56 +0100

    Add TODO (horsTableau.java)

--- src/horsTableau.java
+++ src/horsTableau.java
@@ -1,3 +1,6 @@
+/* TODO Javadoc */
+/* TODO Package. */
+/* TODO Nom de classe avec des majuscule en première lettre. */
 public class horsTableau extends Exception {
-
+    /* TODO En Java, on préférera mettre des Exceptions avec message pour la traçabilité. */
 }
