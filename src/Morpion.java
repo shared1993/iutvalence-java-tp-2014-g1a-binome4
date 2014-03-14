dd24c03f7e7923d46868e35add64b1dc0dbc4a07
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:51:11 +0100

    Add TODO (Morpion.java)

--- src/Morpion.java
+++ src/Morpion.java
@@ -14,28 +14,32 @@
 import java.util.Scanner;
 
 public class Morpion {
-    /* TODO Constantes ? */
-    private static final int vide = 0;
-    private final int pionJoueur1 = 1;
-    private final int pionJoueur2 = 2;
+    /* TODO Constante ? */
+    private static final int VIDE        = 0;
+    /* TODO Constante ? */
+    private final        int pionJoueur1 = 1;
+    /* TODO Constante ? */
+    private final        int pionJoueur2 = 2;
+    /* TODO Est-ce que ça change en cours de partie ? */
     private int[][] tableau;
     private boolean tour;
 
     public Morpion() {
-
-	tableau = new int[][] { { vide, vide, vide }, { vide, vide, vide },
-		{ vide, vide, vide } };
-	tour = true; /*
-		      * tour=true: le joueur 1 joue, tour=false: le joueur 2
+        /* TODO Peut être simplifié en "new int [3][3]" parce que VIDE=0 */
+        tableau = new int[][]{{VIDE, VIDE, VIDE}, {VIDE, VIDE, VIDE}, {VIDE, VIDE, VIDE}};
+        tour = true;
+        /* TODO Dans la JavaDoc ! */
+        /*
+              * tour=true: le joueur 1 joue, tour=false: le joueur 2
 		      * joue
 		      */
     }
 
-    public void jouer(){
-	System.out.println(toString());// Afficher
-	saisir() ;// Saisir
-	poserPion(numLigne,numColonne);// poserPion
-	System.out.println(toString());
+    public void jouer() {
+        System.out.println(toString());// Afficher
+        saisir();// Saisir
+        poserPion(numLigne, numColonne);// poserPion
+        System.out.println(toString());
 	// Victoire ?
     }
 
@@ -44,12 +48,13 @@ public class Morpion {
 	tour = !tour;
     }
 
+    /* TODO Peut être simplifié avec un enum. À discuter. */
     public String toString() {
 	String morpionAsciiArt = "";
 
 	for (int numeroDeLigne = 0; numeroDeLigne < 3; numeroDeLigne++) {
 	    for (int numeroDeColonne = 0; numeroDeColonne < 3; numeroDeColonne++) {
-		if(this.tableau[numeroDeLigne][numeroDeColonne] == vide)
+		if(this.tableau[numeroDeLigne][numeroDeColonne] == VIDE)
 		    morpionAsciiArt += "  ";
 		else
     			if (this.tableau[numeroDeLigne][numeroDeColonne] == pionJoueur1)
@@ -64,8 +69,9 @@ public class Morpion {
 	return morpionAsciiArt;
     }
 
-    public void saisir() {	
-	int numLigne = 4; 
+    /* TODO numLigne et numColonne deviennent des attributs. */
+    public void saisir() {
+	int numLigne = 4;
 	while((numLigne < 0) || (numLigne > 2)){
 	    	Scanner saisieLigne = new Scanner(System.in);
 		System.out.println("Veuillez saisir le numéro de ligne :");
@@ -81,7 +87,7 @@ public class Morpion {
 	    numColonne = Integer.parseInt(colonne);
 	    numColonne = numColonne - 1;
 	}
-	
+
     }
 
 }
