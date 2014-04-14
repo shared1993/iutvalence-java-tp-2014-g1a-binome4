93f028021303ca1e40e10bfd080daa5227456cff
alexdu18 <alexandre.culty@iut-valence.fr>
2014-04-14 20:44:04 +0200

    Update Joueur.java

--- src/fr/iutvalence/tp1a/binome4/morpion/Joueur.java
+++ src/fr/iutvalence/tp1a/binome4/morpion/Joueur.java
@@ -6,19 +6,24 @@ package fr.iutvalence.tp1a.binome4.morpion;
  * @version 1.0.0
  */
 public class Joueur {
+	/** Nom du joueur */
     private final String nom;
     
+    /** Pion du joueur */
     private final Pion pion;
 
+	/** Constructeur du joueur avec le nom et le pion */
     public Joueur(String nom, Pion pion) {
     	this.nom = nom;
 		this.pion = pion;
     }
     
+    /** Accesseur pour le nom du joueur */
     public String nom() {
     	return nom;
     }
     
+    /** Accesseur pour le pion du joueur */
     public Pion pion() {
     	return pion;
     }
