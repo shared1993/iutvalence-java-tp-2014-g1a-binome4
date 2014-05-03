d4535a51635cea462bb2dd163c9a8dfa0d378675
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-03 03:46:27 +0200

    Update Score.

--- src/fr/iutvalence/tp1a/binome4/morpion/Score.java
+++ src/fr/iutvalence/tp1a/binome4/morpion/Score.java
@@ -1,59 +1,60 @@
 package fr.iutvalence.tp1a.binome4.morpion;
 
 /**
+ * Gestionnaire de scores.
+ *
  * @author Prinsac & Culty
- * @version 1.0
+ * @version 1.1
  */
 
-public class Score {
+public final class Score {
     /** Joueur 1. */
-    private final Joueur joueur1;
+    private final Joueur m_joueur1;
 
     /** Joueur 2. */
-    private final Joueur joueur2;
+    private final Joueur m_joueur2;
 
     /** Score du joueur 1. */
-    private int scoreJ1;
+    private int m_scoreJ1;
 
     /** Score du joueur 2. */
-    private int scoreJ2;
+    private int m_scoreJ2;
 
     /** Score Nul. */
-    private int scoreNul;
+    private int m_scoreNul;
 
     /** Constructeur de Score avec les 2 noms des joueurs. */
-    public Score(Joueur joueur1, Joueur joueur2) {
-	this.joueur1 = joueur1;
-	this.joueur2 = joueur2;
-	scoreJ1 = 0;
-	scoreJ2 = 0;
-	scoreNul = 0;
+    public Score(final Joueur joueur1, final Joueur joueur2) {
+        m_joueur1 = joueur1;
+        m_joueur2 = joueur2;
+        m_scoreJ1 = m_scoreJ2 = m_scoreNul = 0;
     }
 
     /** Incremente le score des joueurs. */
-    public void gagne(String joueur) {
-	if (joueur.equals(joueur1.nom()))
-	    scoreJ1++;
-	if (joueur.equals(joueur2.nom()))
-	    scoreJ2++;
+    public void gagne(final String joueur) {
+        if (joueur.equals(m_joueur1.nom())) { m_scoreJ1++; }
+        if (joueur.equals(m_joueur2.nom())) { m_scoreJ2++; }
     }
-    
+
     /** Incremente le score des joueurs. */
-    public void nul(){
-    	scoreNul++;
+    public void nul() {
+        m_scoreNul++;
     }
 
     /** Donne le score de Nul. */
     public int getNul() {
-    	return scoreNul;
+        return m_scoreNul;
     }
-    
+
     /** Donne le score des joueurs. */
-    public int getScore(String joueur) {
-	if (joueur.equals(joueur1.nom()))
-	    return scoreJ1;
-	if (joueur.equals(joueur2.nom()))
-	    return scoreJ2;
-	return -1;
+    public int getScore(final String joueur) {
+        if (joueur.equals(m_joueur1.nom())) { return m_scoreJ1; }
+        if (joueur.equals(m_joueur2.nom())) { return m_scoreJ2; }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Score{m_joueur1=%s, m_joueur2=%s, m_scoreJ1=%d, m_scoreJ2=%d, m_scoreNul=%d}", m_joueur1, m_joueur2, m_scoreJ1, m_scoreJ2, m_scoreNul);
     }
 }
